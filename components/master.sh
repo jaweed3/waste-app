a62eff5860306a0f7a8b74eb750c9894bc907b86
jaweed3 <wedjaw22@gmail.com>
2025-05-17 05:35:44 +0700

    fix: fixing the file name for serving

--- components/master.sh
+++ components/master.sh
@@ -11,16 +11,21 @@ fi
 
 # Starting Tensorflow Serving in the background
 echo "Starting Tensorflow Serving...."
-./start_tf_serving.sh
-TF_SERVING_PID=$
+./serving.sh
+TF_SERVING_PID=$!
 
 # Wait for TF Serving to start
 echo "Waiting Tensorflow Serving to Start"
 sleep 5
 
 # Start Flask App in the background
-echo "Waiting for Flask API....."
-sleep 5
+echo "Starting Flask API....."
+python3 app.py &
+FLASK_PID=$!
+
+# Waiting for Flask API
+echo "Waiting for Flask API to start..."
+sleep 3
 
 # Starting the GUI
 echo "Starting the GUI...."
